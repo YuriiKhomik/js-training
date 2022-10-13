b8b363bf6fdc4e123f455d2c44fe620f489e7475
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-13 19:21:29 +0300

    ..

---
+++ data/products.js
@@ -0,0 +1,58 @@
+export default [
+    {
+        name: 'Steering Wheel',
+        description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, impedit aliquid! Doloribus fugiat libero, voluptatem culpa minima eius esse sapiente.',
+        price: 2000,
+        available: true,
+        onSale: true,
+    },
+      {
+        name: 'Licht Machine',
+        description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, impedit aliquid! Doloribus fugiat libero, voluptatem culpa minima eius esse sapiente.',
+        price: 3000,
+        available: false,
+        onSale: false,
+    },
+    {
+        name: 'Pieso Element',
+        description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, impedit aliquid! Doloribus fugiat libero, voluptatem culpa minima eius esse sapiente.',
+        price: 1500,
+        available: true,
+        onSale: false,
+    },
+    {
+        name: 'Shifter',
+        description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, impedit aliquid! Doloribus fugiat libero, voluptatem culpa minima eius esse sapiente.',
+        price: 2500,
+        available: false,
+        onSale: true,
+    },
+    {
+        name: 'Fruerheizung',
+        description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, impedit aliquid! Doloribus fugiat libero, voluptatem culpa minima eius esse sapiente.',
+        price: 3200,
+        available: true,
+        onSale: true,
+    },
+    {
+        name: 'Obengefohrer',
+        description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, impedit aliquid! Doloribus fugiat libero, voluptatem culpa minima eius esse sapiente.',
+        price: 4000,
+        available: true,
+        onSale: false,
+    },
+        {
+        name: 'Battery',
+        description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, impedit aliquid! Doloribus fugiat libero, voluptatem culpa minima eius esse sapiente.',
+        price: 1000,
+        available: false,
+        onSale: true,
+    },
+]
