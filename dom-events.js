8b616aabf92462168483ae391a77eff0374e2e8a
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-14 13:50:37 +0300

    input events

--- dom-events.js
+++ dom-events.js
@@ -144,4 +144,49 @@
 
 
 
+// // INPUT EVENTS
+
+// // creating object of refs:
+
+// const refs = {
+//     input: document.querySelector('.js-input'),
+//     nameLabel: document.querySelector('.js-button > span'),
+//     licenseCheckbox: document.querySelector('.js-license'),
+//     btn: document.querySelector('.js-button'),
+// };
+
+// // refs.input.addEventListener('focus', onInputFocus);
+// // refs.input.addEventListener('blur', onInputBlur);
+// // // use only for checkboxes etc...
+// // refs.input.addEventListener('change', onInputChange);
+// // // use for text
+// // refs.input.addEventListener('input', onInputChange);
+
+// refs.input.addEventListener('input', onInputChange);
+// refs.licenseCheckbox.addEventListener('change', onLicenseChange);
+
+// // function onInputFocus() {
+// //     console.log('Input got focus - event "focus"')
+// // };
+
+// // function onInputBlur() {
+// //     console.log('Input lost focus - event "blur"')
+// // };
+
+// function onInputChange(event) {
+//     // console.log(event.currentTarget.value);
+//     refs.nameLabel.textContent = event.currentTarget.value;
+// };
+
+// function onLicenseChange(event) {
+//     // console.log(event.currentTarget.checked);
+//     // console.log('refs.btn.disabled: ', refs.btn.disabled);
+    
+//     refs.btn.disabled = !event.currentTarget.checked;
+// };
+
+
+
+
+
 
