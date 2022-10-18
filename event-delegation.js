0971dcc59780a9214e855824b9d092e398f6e3a0
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-18 20:00:48 +0300

    crossbrowser lazyloading

--- event-delegation.js
+++ event-delegation.js
@@ -570,7 +570,7 @@
 // // (if need - use fuse.JS for pukhnastyi poshuk)
 
 
-// // LASYLOAD IMG (we need to add attribute loading="lazy" to every img in HTML)
+// LASYLOAD IMG (we need to add attribute loading="lazy" to every img in HTML)
 
 // const lazyImages = document.querySelectorAll('img[loading="lazy"]');
 // // console.log(lazyImages);
@@ -600,3 +600,31 @@ function onImageLoaded(evt) {
 };
 
 
+// HOW DO I HANDLE NATIVE LAZYLOADING
+
+if ('loading' in HTMLImageElement.prototype) {
+    // якщо браузер підтримує, то ми знаходимо всі ліниві картинки 
+    const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+    
+    // і додаємо тна кожну картинку атрибут img.src і його значенням буде значення img.dataset.src
+    lazyImages.forEach(img => {
+        img.src = img.dataset.src;
+    });
+} else {
+    console.log('browser NOT supports lazyload')
+    
+    const script = document.createElement('script');
+    script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+    script.integrity = "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+    script.crossOrigin = "anonymous";
+    script.referrerPolicy = "no-referrer";
+
+    document.body.appendChild(script);
+}
+
+    // <script
+    //   src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js"
+    //   integrity="sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ=="
+    //   crossorigin="anonymous"
+    //   referrerpolicy="no-referrer"
+    // ></script>
