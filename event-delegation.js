cf31c68db6508324bbd11d79102455f8e980187f
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-18 12:51:03 +0300

    chatty events

--- event-delegation.js
+++ event-delegation.js
@@ -164,55 +164,55 @@
 // };
 
 
-// COLOR-PICKER
+// // COLOR-PICKER
 
-const clrs = [
-    { hex: '#f44336', rgb: '244,67,54' },
-    { hex: '#e91e63', rgb: '233,30,99' },
-    { hex: '#9c27b0', rgb: '156,39,176' },
-    { hex: '#673ab7', rgb: '103,58,183' },
-    { hex: '#3f51b5', rgb: '63,81,181' },
-    { hex: '#2196f3', rgb: '33,150,243' },
-    { hex: '#00bcd4', rgb: '0,188,212' },
-    { hex: '#009688', rgb: '0,150,136' },
-    { hex: '#4caf50', rgb: '76,175,80' },
-    { hex: '#ffeb3b', rgb: '255,235,59' },
-    { hex: '#ff9800', rgb: '255,152,0' },
-    { hex: '#795548', rgb: '121,85,72' },
-    { hex: '#607d8b', rgb: '96,125,139' },
-];
+// const clrs = [
+//     { hex: '#f44336', rgb: '244,67,54' },
+//     { hex: '#e91e63', rgb: '233,30,99' },
+//     { hex: '#9c27b0', rgb: '156,39,176' },
+//     { hex: '#673ab7', rgb: '103,58,183' },
+//     { hex: '#3f51b5', rgb: '63,81,181' },
+//     { hex: '#2196f3', rgb: '33,150,243' },
+//     { hex: '#00bcd4', rgb: '0,188,212' },
+//     { hex: '#009688', rgb: '0,150,136' },
+//     { hex: '#4caf50', rgb: '76,175,80' },
+//     { hex: '#ffeb3b', rgb: '255,235,59' },
+//     { hex: '#ff9800', rgb: '255,152,0' },
+//     { hex: '#795548', rgb: '121,85,72' },
+//     { hex: '#607d8b', rgb: '96,125,139' },
+// ];
 
-// посилання на батьківський контейнер
-const paletteContainer = document.querySelector('.js-palette');
-// змінна для сформованої розмітки із всіма кольорами
-const cardsMarkup = createColorCardsMarkup(clrs);
-// вставляємо цю розмітку в батьківський контейнер
-paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+// // посилання на батьківський контейнер
+// const paletteContainer = document.querySelector('.js-palette');
+// // змінна для сформованої розмітки із всіма кольорами
+// const cardsMarkup = createColorCardsMarkup(clrs);
+// // вставляємо цю розмітку в батьківський контейнер
+// paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
-paletteContainer.addEventListener('click', onPaletteContainerClick)
+// paletteContainer.addEventListener('click', onPaletteContainerClick)
 
 
 
-function createColorCardsMarkup(colors) {
-    return colors.map(({hex, rgb}) => {
-        return `
-    <div class="color-card">
-        <div
-            class="color-swatch"
-            data-hex="${hex}"
-            data-rgb="${rgb}"
-            style="background-color: ${hex}"
-        ></div>
-        <div class="color-meta">
-            <p>HEX: ${hex}</p>
-            <p>RGB: ${rgb}</p>
-        </div>
-    </div>
-    `
-        ;
-    })
-        .join('');
-}
+// function createColorCardsMarkup(colors) {
+//     return colors.map(({hex, rgb}) => {
+//         return `
+//     <div class="color-card">
+//         <div
+//             class="color-swatch"
+//             data-hex="${hex}"
+//             data-rgb="${rgb}"
+//             style="background-color: ${hex}"
+//         ></div>
+//         <div class="color-meta">
+//             <p>HEX: ${hex}</p>
+//             <p>RGB: ${rgb}</p>
+//         </div>
+//     </div>
+//     `
+//         ;
+//     })
+//         .join('');
+// }
 
 // console.log(createColorCardsMarkup(clrs));
 
@@ -259,47 +259,47 @@ function createColorCardsMarkup(colors) {
 //     // console.log(evt.target.dataset.hex);    
 // } 
 
-//  в попередній функції надто багато завдань
-// розбиваємо її на менші фенкції:
+// //  в попередній функції надто багато завдань
+// // розбиваємо її на менші фенкції:
 
-function onPaletteContainerClick(evt) {
-    // отримуємо посилання на елемент
-    const isColorSwatchEl = evt.target.classList.contains('color-swatch');
+// function onPaletteContainerClick(evt) {
+//     // отримуємо посилання на елемент
+//     const isColorSwatchEl = evt.target.classList.contains('color-swatch');
 
-    // фільтруємо події виключно на цьому елементі
-    if (!isColorSwatchEl) {
-        return;
-    };
+//     // фільтруємо події виключно на цьому елементі
+//     if (!isColorSwatchEl) {
+//         return;
+//     };
 
-    // змінна для елементу, по якому відбулась подія
-    const swatchEl = evt.target;
-    // змінна для його батьківського елементу з потрібним класом
-    const parentColorCard = swatchEl.closest('.color-card');
+//     // змінна для елементу, по якому відбулась подія
+//     const swatchEl = evt.target;
+//     // змінна для його батьківського елементу з потрібним класом
+//     const parentColorCard = swatchEl.closest('.color-card');
     
-    // видаляємо активний клас
-    removeActiveCardClass()
-    // додаємо активний клас
-    addActiveCardClass(parentColorCard)
-    // передаємо значення активного кольору на колір body
-    setBodyBgColor(swatchEl.dataset.hex)
-};
+//     // видаляємо активний клас
+//     removeActiveCardClass()
+//     // додаємо активний клас
+//     addActiveCardClass(parentColorCard)
+//     // передаємо значення активного кольору на колір body
+//     setBodyBgColor(swatchEl.dataset.hex)
+// };
 
 
-function setBodyBgColor (color){
-     document.body.style.backgroundColor = color;
-};
+// function setBodyBgColor (color){
+//      document.body.style.backgroundColor = color;
+// };
 
-function removeActiveCardClass() {
-    // шукаємо активну картку:
-    const currentActiveCard = document.querySelector('.color-card.is-active');
+// function removeActiveCardClass() {
+//     // шукаємо активну картку:
+//     const currentActiveCard = document.querySelector('.color-card.is-active');
 
-    // забираємо активний клас
-    if (currentActiveCard) {
-        currentActiveCard.classList.remove('is-active')
-    };
-};
+//     // забираємо активний клас
+//     if (currentActiveCard) {
+//         currentActiveCard.classList.remove('is-active')
+//     };
+// };
 
-function addActiveCardClass(card) {
-    card.classList.add('is-active');
-};
+// function addActiveCardClass(card) {
+//     card.classList.add('is-active');
+// };
 
